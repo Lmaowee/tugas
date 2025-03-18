61573483af1b48219b51010439b29a84300ab267
Lmaowee <nelsonphang5473@gmail.com>
2025-03-18 18:32:29 +0700

    menambahkan function untuk menambahkan textbox

---
+++ New folder/php/component.php
@@ -0,0 +1,13 @@
+<?php 
+  function inputElement() {
+    $ele = "<div class=\"input-group mb-2\">
+              <div class=\"input-group-prepend\">
+                <div class=\"input-group-text bg-warning\"><i class=\"fa-solid fa-circle-user\"></i></div>
+              </div>
+              <input type=\"text\" class=\"form-control\" id=\"inlineFormInputGroup\" placeholder=\"ID\">
+            </div>";
+
+            echo $ele;
+  }
+  
+?>
