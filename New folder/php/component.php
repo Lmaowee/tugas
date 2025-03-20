649f10eda98ba5681e51a9c5679074bd2819526e
Lmaowee <nelsonphang5473@gmail.com>
2025-03-20 22:44:44 +0700

    adds a new button function

--- New folder/php/component.php
+++ New folder/php/component.php
@@ -10,5 +10,13 @@
 
             echo $ele;
   }
+
+  function buttonElement ($btnid, $styleclass,$text,$name,$attr) {
+    $btn = "
+    <button name='$name' '$attr' class='$styleclass' id='$btnid'>$text</button>
+    ";
+
+    echo $btn;
+  }
   
 
