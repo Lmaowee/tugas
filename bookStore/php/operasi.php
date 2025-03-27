61c3460cf80c48d6830dec7d45be3afd66585743
Lmaowee <nelsonphang5473@gmail.com>
2025-03-27 22:02:53 +0700

    create operation

---
+++ bookStore/php/operasi.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once("database.php");
+require_once("component.php");
+
+$con = createDataBase();
+
+
+// create
+
+if (isset($_POST['create'])) {
+  createData();
+}
+
+function createData(){
+  $namaBuku = textboxValue(value:"nama_buku");
+  $authorBuku = textboxValue(value:"nama_author");
+  $hargaBuku = textboxValue(value:"Harga_Buku");
+
+  if($namaBuku && $authorBuku && $hargaBuku){
+    $sql = "INSERT INTO books(nama_buku, nama_author, Harga_Buku)
+            VALUES ('$namaBuku', 
+                    '$authorBuku',
+                    '$hargaBuku')";
+            
+  
+    if(mysqli_query($GLOBALS['con'],$sql)){
+      echo "record seucessfull inserted";
+    }else{
+      echo "error";
+    }
+
+  }else{
+    echo "provide data in the textbox";
+  }
+}
+
+function textboxValue($value){
+  $textbox = mysqli_real_escape_string($GLOBALS['con'], trim($_POST[$value]));
+  if (empty($textbox)) {
+    return false;
+  }
+
+  else {
+    return $textbox;
+  }
+}
